e5993bf561aebdf49b6a1746930bb8c32abdd563
qun <qunwang6@gmail.com>
2023-10-30 11:43:42 +0800

    aaa

---
+++ offline.js
@@ -0,0 +1,24 @@
+﻿{
+	"version": 1576342640,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/canvas.png",
+		"images/fb-sheet0.png",
+		"images/tw-sheet0.png",
+		"images/b_ready-sheet0.png",
+		"images/promo-sheet0.png",
+		"images/arrows-sheet0.png",
+		"images/sprite-sheet0.png",
+		"media/down.m4a",
+		"media/down.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
